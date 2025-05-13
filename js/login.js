e4b8017ba0a3f697804ed9f330f837ae606aa8c4
eyanprogh <ryannaosei329@gmail.com>
2025-05-13 19:39:25 -0300

    primeiro commit

---
+++ js/login.js
@@ -0,0 +1,6 @@
+function calcular() {
+    const p1 = parseFloat(document.getElementById("mauricio").value);
+    const p2 = parseFloat(document.getElementById("malta").value);
+    const rf = p1 + p2;
+
+}
